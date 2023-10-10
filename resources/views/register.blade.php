abdeefa394a478c45adb39a52ed9d0d71485ef91
rprasetya <rayaprasetya90@gmail.com>
2023-10-11 00:12:42 +0800

    register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,15 +1,20 @@
 @extends('layout.base')
 @section('content')
+
     <div class="formLoginCont">
+        <div class="backToLoginBtn">
+            <a href="/login">
+                <img src="https://img.icons8.com/metro/52/back.png" alt="back"/>
+            </a>
+        </div>
         <form class="form">
             <div class="titleLogin">Welcome,<br><span>Provide your information below</span></div>
+            <input class="input" name="fullName" placeholder="Full Name" type="text" required="">
+            <input class="input" name="email" placeholder="E-mail" type="email" required="">
             <input class="input" name="username" placeholder="Username" type="text" required="">
             <input class="input" name="password" placeholder="Password" type="password" required="">
-            <div class="register">
-              <a href="/register">Register ?</a>
-            </div>
             <div class="btn-confirm">
-                <button class="button-confirm">Let`s go →</button>
+                <button class="button-confirm">Sign Up</button>
             </div>
           </form>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/contact', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/register', function () {
+    return view('register');
+});
